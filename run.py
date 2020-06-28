92820a7c2b957b140055d0b40384c25dfbb7fffc
Mercy <aprilkasha42@gmail.com>
2020-06-28 20:46:33 +0300

    use method create_user function to for user to crete an account

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
 from user import User
 from credential import Credential
+
+def create_user(name, password):
+    '''
+    Function to create a user account
+
+    Args:
+        name : the name the user wants for the account
+        password : the password the user want for the account
+    '''
+
+    new_user = User(name,password)
+
+    return new_user
