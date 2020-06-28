17332ef321392933beabce0f6b06af0a7f13d3c9
Mercy <aprilkasha42@gmail.com>
2020-06-28 19:50:43 +0300

    create init method to test if all objects are initiated properly

--- credential.py
+++ credential.py
@@ -5,3 +5,18 @@ class Credential:
     '''
     Class that generates instances of a users credentials
     '''
+
+    credential_list = []
+
+def __init__(self, user_password, credential_name, credential_password):
+        '''
+        __init__ method to define the properties of a User object
+
+        Args:
+            user_password : password of the user
+            credential_name : name of an account
+            credential_password : password for the account
+        '''
+        self.user_password = user_password
+        self.credential_name = credential_name
+        self.credential_password = credential_password
