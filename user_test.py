579106af0334db38f755280a0cacef2158b0018a
Mercy <aprilkasha42@gmail.com>
2020-06-28 12:48:46 +0300

    We add another test case test_save_multiple_usder to test if we can save multiple users in our user list.

--- user_test.py
+++ user_test.py
@@ -11,24 +11,32 @@ class TestUser(unittest.TestCase):
     '''
 
     def setUp(self):
-    '''
-    Set up method to run before each test case
-    '''
+        '''
+        Set up method to run before each test case
+        '''
 
-    self.new_user = User("James","Nyugi")
+        self.new_user = User("James","Nyugi")
 
     def test_init(self):
-    '''
-    Test case to test if the object is initialised properly
-    '''
+        '''
+        Test case to test if the object is initialised properly
+        '''
 
-    self.assertEqual( self.new_user.user_name, "James" )
-    self.assertEqual( self.new_user.user_password, "Nyugi" )
+        self.assertEqual( self.new_user.user_name, "James" )
+        self.assertEqual( self.new_user.user_password, "Nyugi" )
 
     def test_save_user(self):
-    '''
-    Test case to test if the user object is saved into the user list
-    '''
-    self.new_user.save_user()
+        '''
+        Test case to test if the user object is saved into the user list
+        '''
+        self.new_user.save_user()
 
-    self.assertEqual( len(User.user_list), 1 )
+        self.assertEqual( len(User.user_list), 1 )
+
+    def test_save_multiple_users(self):
+        '''
+        Test case to test if you can save multiple objects to user list
+        '''
+
+if __name__ ==  '__main__':
+    unittest.main()
