2ae0dd84b2a53f51fb11824e7f545ce314fd24c7
Mercy <aprilkasha42@gmail.com>
2020-06-28 12:01:08 +0300

    check the length of our user_list list to confirm an addition user has been added.

--- user_test.py
+++ user_test.py
@@ -26,6 +26,9 @@ class TestUser(unittest.TestCase):
     self.assertEqual( self.new_user.user_password, "Nyugi" )
 
     def test_save_user(self):
-        '''
-        Test case to test if the user object is saved into the user list
-        '''
+    '''
+    Test case to test if the user object is saved into the user list
+    '''
+    self.new_user.save_user()
+
+    self.assertEqual( len(User.user_list), 1 )
