a121a28fca472beeb23e381046ed32505d7edf85
Mercy <aprilkasha42@gmail.com>
2020-06-28 20:47:45 +0300

    funcion to save user account

--- run.py
+++ run.py
@@ -13,3 +13,13 @@ def create_user(name, password):
     new_user = User(name,password)
 
     return new_user
+
+def save_users(user):
+    '''
+    Function to save a user account
+
+    Args:
+        user : the user account to be saved
+    '''
+
+    user.save_user()
