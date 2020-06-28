62ffcfb57737243fd721251fbe44bb8cf3cb6941
Mercy <aprilkasha42@gmail.com>
2020-06-28 22:00:06 +0300

    use if statement to create a password locker account

--- run.py
+++ run.py
@@ -125,3 +125,15 @@ Use these short codes to get around''')
 
     
         short_code = input().lower()
+
+  if short_code == 'cu':
+            '''
+            Creating a Password Locker account
+            '''
+
+            print("\n")
+            print("New Password Locker Account")
+            print("-"*10)
+
+            print("User name ...")
+            user_name = input()
