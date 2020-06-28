b57e5e341d0b2219f4e6dd52a97cdbbe35c3affd
Mercy <aprilkasha42@gmail.com>
2020-06-28 10:57:58 +0300

    create a subclass class called TestContacts, that inherits from unittest.TestCase

--- user_test.py
+++ user_test.py
@@ -1,2 +1,11 @@
 import unittest
 from user import User
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the User Class behaviours
+
+    Args:
+        unittest.TestCase : Test case class that helps create test cases
+    '''
