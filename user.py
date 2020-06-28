2832d251af6d4f4114c5a67a3b324bca3408115b
Mercy <aprilkasha42@gmail.com>
2020-06-28 10:48:15 +0300

    create an empty list of users

--- user.py
+++ user.py
@@ -2,3 +2,15 @@ class User:
     '''
     Class that generates instances of user accounts
     '''
+    user_list = []
+    def __init__(self, user_name, user_password):
+        '''
+        __init__ method to define the properties of a User object
+
+        Args:
+        user_name : name of a user
+        user_password : password for a user
+       '''
+
+        self.user_name = user_name
+        self.user_password = user_password
